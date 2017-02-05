34809e91d5bb79c8a45caf7bc87d8cb136a6dcd1
chauberahul123 <rahul.chaube2550@gmail.com>
2017-02-05 23:33:19 +0530

    module 4 solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,26 +8,26 @@
     $scope.itemString = "";
     $scope.nbOfItems = 0;
     $scope.resultString;
-     $scope.resultClass;
+    // $scope.resultClass;
     $scope.resultOk = null;
 
     $scope.checkItems = function() {
-      If anything entered in textbox
+      //If anything entered in textbox
       if($scope.itemString.length > 0) {
         console.log("Items entered: " + $scope.itemString);
         var itemArray = $scope.itemString.split(",");
         console.log("Items as array: " + itemArray);
 
         $scope.resultString = checkItemCount(itemArray);
-         var result = checkItemCount(itemArray);
-         $scope.resultString = result.string;
-         $scope.resultClass = result.class;
+        // var result = checkItemCount(itemArray);
+        // $scope.resultString = result.string;
+        // $scope.resultClass = result.class;
         $scope.resultOk = true;
       }
       else
       {
         $scope.resultString = "Please enter data first";
-         $scope.resultClass = "notOk";
+        // $scope.resultClass = "notOk";
         $scope.resultOk = false;
       }
     };
@@ -38,10 +38,10 @@
     console.log("Number of items entered: " + itemCount);
 
     if(itemCount < 4)
-       return { string: "Enjoy!", class: "ok" };
+      // return { string: "Enjoy!", class: "ok" };
       return "Enjoy!";
     else
-       return { string: "Too much!", class: "notOk" };
+      // return { string: "Too much!", class: "notOk" };
       return "Too much!";
   }
 })();
